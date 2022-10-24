d798d3c30710dc12008d35ad3c0a289be64a6858
Klug737714 <63925928+Klug737714@users.noreply.github.com>
2022-10-24 12:56:18 +0300

    Fixing landing page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,33 +7,37 @@ class Home extends Component {
   render() {
     return (
       <React.Fragment>
-        <div className="border-dark bg-light p-5">
-          <h1 className="text-center align-middle mt-50">
-            Practice your skills. Collaborate. Gain experience.
-          </h1>
-          <h4 className="text-center w-50 ms-auto me-auto mt-5">
-            This is a platform for entry level candidates to collaborate
-            together or short term projects on a volunteer basis. You’ll gain
-            experience, a prokect to add to your portfolio, learn how to work as
-            a team and become more prepared for your interviews and future jobs
-          </h4>
-          <div className="d-flex mt-50">
-            <Link
-              className="btn btn-outline-dark mx-auto me-3 col-2"
-              to="/create-project"
-            >
-              CREATE A PROJECT
-            </Link>
-            <Link
-              className="btn btn-outline-dark mx-auto ms-3 col-2"
-              to="/projects"
-            >
-              JOIN A PROJECT
-            </Link>
+        <div className="border-primary bg-image pb-large">
+          <div className="col-12 p-5"></div>
+          <div className="bg-blur col-10 mx-auto">
+            <h1 className="text-center align-middle">
+              Practice your skills. Collaborate. Gain experience.
+            </h1>
+            <h4 className="text-center w-50 ms-auto me-auto mt-5">
+              This is a platform for entry level candidates to collaborate
+              together on short term projects on a volunteer basis. You’ll gain
+              experience, a project to add to your portfolio, learn how to work
+              as a team and become more prepared for your interviews and future
+              jobs
+            </h4>
+            <div className="d-flex mt-5">
+              <Link
+                className=" btn btn-warning text-light mx-auto me-5 col-2 p-3"
+                to="/create-project"
+              >
+                CREATE A PROJECT
+              </Link>
+              <Link
+                className="btn btn-primary mx-auto ms-5 col-2 p-3"
+                to="/projects"
+              >
+                JOIN A PROJECT
+              </Link>
+            </div>
           </div>
         </div>
         <div>
-          <h2 className="text-center mt-3 col-12">HOW DOES IT WORK?</h2>
+          <h2 className="text-center col-12">HOW DOES IT WORK?</h2>
           <div className="d-flex">
             <div className="col-5 bg-light mx-auto me-auto p-5">
               <h1 className="text-center">You have an idea?</h1>
